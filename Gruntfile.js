297dd84d9348bbbb5493f0f80d9d378d65243e3e
Daria Slovetskikh <mdasha1979@yandex.ru>
2015-03-31 21:42:43 +0400

    Автоматизация проекта

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,10 +23,133 @@ module.exports = function(grunt) {
           showCodes: true
         }
       }
-    }
+    }, 
+	// Less to css
+	less: {
+		style: {
+			files: {
+				"build/css/style.css": ["source/less/style.less"]
+			}
+		}
+	},
+	
+	// Autoprefixer  
+	
+	autoprefixer: {
+		options: {
+			browsers: ['last 2 versions', 'ie 8', 'ie 9']
+		},
+		style: {
+			src:"build/css/style.css"
+		}
+	},
+	
+	//  Combine-media-queries  
+	
+	cmq: {
+		styles: {
+			files: {
+				"build/css/style.css": ["build/css/style.css"]
+			}
+		}
+	},
+	
+	//  Cssmin 
+	
+	cssmin: {
+		style: {
+			options: {
+				KeepSpecialComments:  0,
+				report:  "gzip"
+			},
+			files: {
+				"build/css/style.min.css": ["build/css/style.css"]
+			}
+		}
+	
+	},
+	
+	// Csscomb
+	
+	csscomb: {
+		style: {
+			expand:  true,
+			src:["less/**/*.less"]
+		}
+	},
+	
+	// imagemin
+	
+	imagemin: {
+		images: {
+			options: {
+				optimizationlevel:  3
+			},
+			files: [{
+				expand:  true,
+				src:  ["build/img/**/*.{png, jpg, gif, svg}"]
+			}]
+		}
+	},
+	
+	//htmlmin  
+	
+	htmlmin:  {
+		dist: {
+			options: {
+				removeComments:  true,
+				collapseWhitespace: true,
+				caseSensitive:  true,
+				keepClosingSlash:  false
+			},
+			files: {
+				'build/index.min.html': ['build/index.html']
+			}
+		}
+	},   
+	
+	
+	// copy
+	
+		copy:  {
+			build: {
+				files:  [{
+					expand:  true,
+					cwd:  "source",
+					src:  [
+						"img/**",
+						"js/**",
+						"index.html",
+						"octicons/**"
+					],
+					dest:  "build"
+				}]
+			}
+		},
+		
+	// clean  
+	
+		clean:  {
+			build: ["build"]
+		}
+	
   });
 
-  grunt.loadNpmTasks('grunt-lintspaces');
+  
+  require ("load-grunt-tasks") (grunt);
 
   grunt.registerTask('lint', ['lintspaces']);
+  grunt.registerTask("build", [
+  "clean",
+  "copy",
+  "less",
+  "autoprefixer",
+  "cmq",
+  "cssmin",
+  "csscomb",
+  "imagemin",
+  "htmlmin"
+  
+  
+  ]);
 };
